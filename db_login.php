d982001fd3a62682b5403e5d3aac6c7b86520434
aseemkurhekar <45918395+aseemkurhekar@users.noreply.github.com>
2019-02-22 03:56:56 +0530

    Update db_login.php

--- db_login.php
+++ db_login.php
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <?php
-	$db_host='db4free.net';
+	$db_host='db4free.net:3306';
 	$db_username='aseem_kurhekar';
 	$db_password='tudomeda';
 ?>
